6f95ff15fb2f550f6ff18f62c3c6520108004d72
Nathan Thomas <thomanat@iu.edu>
2022-03-25 15:02:20 -0400

    Change master to main in release.config.js

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  branches: "master",
+  branches: "main",
   repositoryUrl: "https://github.com/nfective/ci-cd-test-app",
   plugins: [
     "@semantic-release/commit-analyzer",
