5dcf4c9e9dc478c8f703d9a6baeff3ac4d50980a
Nathan Thomas <thomanat@iu.edu>
2022-03-29 11:43:33 -0400

    feat: adding additional core options

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -12,6 +12,16 @@ try {
 
   core.setOutput("time", time.toTimeString());
 
+  core.startGroup("This starts the beginnning of the group")
+
+  console.log("I am grouped")
+
+  core.endGroup()
+
+  core.setSecret("I am a secret")
+
+  core.exportVariable("Greet", "Hello")
+
   console.log(JSON.stringify(github, null, "\t"));
 } catch (error) {
   core.setFailed(error.message);
