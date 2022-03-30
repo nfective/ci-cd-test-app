c1486b446e3606134d03c85ed17905f98f82c881
Nathan Thomas <thomanat@iu.edu>
2022-03-29 21:58:00 -0400

    test: new attempt with octokit

--- .github/actions/issue/index.js
+++ .github/actions/issue/index.js
@@ -9,30 +9,16 @@ let run = async () => {
         const title = core.getInput('title')
         const body = core.getInput('body')
         const assignees = core.getInput('assignees')
-        
-        const octokit = new github.GitHub(token)
 
-        const octo = new github.getOctokit(token)
+        const octokit = new github.getOctokit(token)
 
-        // Bad Code
-        /*
-
-        const octokit = new github.GitHub(token)
-
-        const response = await octokit.create({
+        const { data: response } = await octokit.rest.issues({
             owner: github.context.repo.owner,
             repo: github.context.repo.repo,
             title: title,
             body: body,
-            assignees: assignees ? assignees.split('\n') : undefined
-        })        
-
-        */
-
-        // const octokit = new github.getOctokit(token)
-
-        // console.log(JSON.stringify(octokit, null,'\n'))
-
+            assignees: assignees ? assignees.split('\n') : undefined            
+        })
 
         core.setOutput('issue', JSON.stringify(response.data))
     } catch (error)
