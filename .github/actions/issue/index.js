5a53e5c18a459b31ead3d1f820aff04f938263c8
Nathan Thomas <thomanat@iu.edu>
2022-03-29 21:27:41 -0400

    fix: observing octokit object

--- .github/actions/issue/index.js
+++ .github/actions/issue/index.js
@@ -10,7 +10,10 @@ let run = async () => {
         const body = core.getInput('body')
         const assignees = core.getInput('assignees')
 
-        const octokit = new github.getOctokit(token)
+        // Bad Code
+        /*
+
+        const octokit = new github.GitHub(token)
 
         const response = await octokit.create({
             owner: github.context.repo.owner,
@@ -18,7 +21,13 @@ let run = async () => {
             title: title,
             body: body,
             assignees: assignees ? assignees.split('\n') : undefined
-        })
+        })        
+
+        */
+
+        const octokit = new github.getOctokit(token)
+
+        console.log(JSON.stringify(octokit, null,'\n'))
     
         core.setOutput('issue', JSON.stringify(response.data))
     } catch (error)
